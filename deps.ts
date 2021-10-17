c7e9c488e93e0652aee05eab8e116c3763046b6b
Raido Kaldma <raido.kaldma@hammertime.ee>
2021-10-17 12:33:37 +0300

    Puppeteer 9.0.2 fixes "Puppeteer was unable to kill the process which ran the browser binary" with Windows + WSL2

--- deps.ts
+++ deps.ts
@@ -7,8 +7,8 @@ export {
 export { dirname, join } from "https://deno.land/std@0.95.0/path/mod.ts";
 export { delay } from "https://deno.land/std@0.95.0/async/mod.ts";
 export { v4 as uuid } from "https://deno.land/std@0.95.0/uuid/mod.ts";
-export { Browser, Page } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
-export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
+export { Browser, Page } from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
+export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
 export { Table } from "https://deno.land/x/cliffy@v0.18.2/table/mod.ts";
 export { default as ky } from "https://cdn.skypack.dev/ky?dts";
 export { default as Kia } from "https://deno.land/x/kia@0.3.0/mod.ts";
